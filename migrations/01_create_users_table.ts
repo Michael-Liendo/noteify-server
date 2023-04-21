5f66999d86f4a41eefdc3cd6950cee3ee698cf72
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-04-21 15:42:12 -0400

    feat: uuid autogenerate

--- migrations/01_create_users_table.ts
+++ migrations/01_create_users_table.ts
@@ -1,15 +1,15 @@
 import { Knex } from 'knex';
-
-exports.up = function (knex: Knex) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments('id').primary();
+exports.up = async (knex: Knex) => {
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
     table.string('full_name').notNullable();
-    table.string('email').notNullable().unique();
+    table.string('email').unique().notNullable();
     table.string('password').notNullable();
     table.timestamps(true, true);
   });
 };
 
-exports.down = function (knex: Knex) {
-  return knex.schema.dropTable('users');
+exports.down = async (knex: Knex) => {
+  await knex.schema.dropTableIfExists('users');
 };
--- migrations/02_create_notes_table.ts
+++ migrations/02_create_notes_table.ts
@@ -3,11 +3,7 @@ import { Knex } from 'knex';
 exports.up = function (knex: Knex) {
   return knex.schema.createTable('notes', (table) => {
     table.increments('id').primary();
-    table
-      .integer('user_id')
-      .unsigned()
-      .references('users.id')
-      .onDelete('CASCADE');
+    table.uuid('user_id').unsigned().references('users.id').onDelete('CASCADE');
     table.string('title').notNullable();
     table.text('content').notNullable();
     table.timestamps(true, true);
@@ -15,5 +11,5 @@ exports.up = function (knex: Knex) {
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable('notes');
+  return knex.schema.dropTableIfExists('notes');
 };
