528e49cdc2d66d94a2fdf781af739d196d423ca0
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-04-20 23:14:24 -0400

    feat: connection to the database

--- src/domain/database.ts
+++ src/domain/database.ts
@@ -3,7 +3,7 @@ import knex from 'knex';
 const database = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    host: '0.0.0.0',
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import Fastify from 'fastify';
+import database from './domain/database';
 const fastify = Fastify();
 
-fastify.listen({ port: 3000 }, function (err, address) {
-  if (err) {
-    console.error(err);
+database
+  .raw('select 1')
+  .then(() =>
+    console.log('Connection to database has been established successfully')
+  )
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
     process.exit(1);
+  });
+
+fastify.listen(
+  { port: Number(process.env.PORT) || 3000 },
+  function (err, address) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server is now listening on ${address}`);
   }
-  console.log(`Server is now listening on ${address}`);
-});
+);
