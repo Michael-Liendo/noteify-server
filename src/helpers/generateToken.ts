8cb55cdab042288e12deb6215b9cf7550a92d8a7
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-05-02 13:47:59 -0400

    chore: change expire time

--- src/helpers/generateToken.ts
+++ src/helpers/generateToken.ts
@@ -3,6 +3,7 @@ import { User } from '../types/User';
 
 export default function generateToken(user: User) {
   const payload = { id: user.id, email: user.email };
-  const options = { expiresIn: '1h' };
+  const options = { expiresIn: '15d' };
+
   return jwt.sign(payload, process.env.JWT_SECRET, options);
 }
